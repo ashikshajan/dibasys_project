6196d7126cc9481b583bf5ecc715090ad2eb2b2c
ashikshajan <ashikshajan1@gmail.com>
2025-07-17 12:14:15 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# dibasys_project
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/dibasys_project/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dibasys_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
